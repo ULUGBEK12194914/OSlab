3f22165ca284f3ca5969cda667b20452352c8fe3
ULUGBEK12194914 <mirzabakhromov2001@gmail.com>
2023-03-09 03:18:13 +0900

    Your comming

---
+++ week2.sh
@@ -0,0 +1,18 @@
+   1  cd ~ 
+    2  cd ~
+    3  sudo apt-get update
+    4  sudo apt-get upgrade
+    5  cd ~
+    6  sudo apt update && sudo apt upgrade -y
+    7  touch test.txt
+    8  nano test.txt
+    9  cd OSlab
+   10  cd Dekstop/OSlab
+   11  cd Desktop
+   12  ls
+   13  cd OSlab
+   14  cd ~
+   15  cd /Desktop/OSlab
+   16  cd /Desktop/OSlab/
+   17  cd Desktop/OSlab/
+   18  history
